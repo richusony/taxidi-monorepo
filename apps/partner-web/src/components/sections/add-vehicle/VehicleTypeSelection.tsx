5e5f2758bcba59a23b2e932e84d88ef2953e600f
Richu Sony <sonyrichu4@gmail.com>
2026-03-19 12:41:53 +0530

    wip: created vehicle type selection ui

--- apps/partner-web/src/components/sections/add-vehicle/VehicleTypeSelection.tsx
+++ apps/partner-web/src/components/sections/add-vehicle/VehicleTypeSelection.tsx
@@ -1,17 +1,61 @@
-import type { VehicleAddWizardType } from '@/pages/add-vehicle/page';
+import type {
+  VehicleAddWizardType,
+  VehicleTypesType,
+} from '@/pages/add-vehicle/page';
+import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
 
-const VehicleTypeSelction = ({ nextStep, prevStep }: VehicleAddWizardType) => {
+interface TypesProps extends VehicleAddWizardType {
+  vehicleTypes: VehicleTypesType[];
+}
+
+const VehicleTypeSelection = ({
+  nextStep,
+  prevStep,
+  vehicleTypes,
+}: TypesProps) => {
   return (
-    <section>
-      <h3>Select Vehicle Type</h3>
-      <form>
-        <div className="flex gap-x-3">
-          <button onClick={prevStep}>Go back</button>
-          <button onClick={nextStep}>Next</button>
-        </div>
-      </form>
+    <section className="rounded-2xl border border-white/10 p-8 bg-white/10 ">
+      <h3 className="font-semibold">What type of vehicle?</h3>
+      <p className="mt-1 text-sm text-white/40">
+        Select the category that best describes your vehicle.
+      </p>
+      <hr className="my-5 text-white/10" />
+
+      <div className='flex flex-wrap gap-5'>
+        {vehicleTypes.map((vt, index) => (
+          <VehicleTypeComponent key={index + 'vt-index'} vehicleType={vt} />
+        ))}
+      </div>
+
+      <hr className="my-5 text-white/10" />
+      <div className="flex justify-between">
+        <button onClick={prevStep} className={`flex items-center gap-x-1`}>
+          <BiLeftArrowAlt />
+          <span>Back</span>
+        </button>
+        <button onClick={nextStep} className={`flex items-center gap-x-1`}>
+          <span>Continue</span>
+          <BiRightArrowAlt />
+        </button>
+      </div>
     </section>
   );
 };
 
-export default VehicleTypeSelction;
+export default VehicleTypeSelection;
+
+const VehicleTypeComponent = ({
+  vehicleType,
+}: {
+  vehicleType: VehicleTypesType;
+}) => {
+  return (
+    <div
+      className={`transition-all ease-in w-52 h-28 rounded-xl p-5 bg-black/30 flex flex-col items-center text-white/40 hover:scale-105`}
+    >
+      <p>{vehicleType.icon}</p>
+      <p className={`mt-1 text-white/80 text-sm`}>{vehicleType.title}</p>
+      <p className="mt-1 text-xs">{vehicleType.subtitle}</p>
+    </div>
+  );
+};
--- apps/partner-web/src/pages/add-vehicle/page.tsx
+++ apps/partner-web/src/pages/add-vehicle/page.tsx
@@ -1,17 +1,27 @@
-import { useState, type SyntheticEvent } from 'react';
+import { useState, type JSX, type SyntheticEvent } from 'react';
 import VehicleConfirm from '@/components/sections/add-vehicle/VehicleConfirm';
-import VehicleTypeSelction from '@/components/sections/add-vehicle/VehicleTypeSelection';
 import VehicleModelSelction from '@/components/sections/add-vehicle/VehicleModelSelection';
 import VehicleBrandSelction from '@/components/sections/add-vehicle/VehicleBrandSelection';
 import VehicleDetailsSelction from '@/components/sections/add-vehicle/VehicleDetailsSelection';
 import VehiclePricingSelction from '@/components/sections/add-vehicle/VehiclePricingSelection';
 import VehicleVariantSelction from '@/components/sections/add-vehicle/VehicleVariantSelection';
 import VehicleLocationSelction from '@/components/sections/add-vehicle/VehicleLocationSelection';
+import ProgressTracking from '@/components/sections/add-vehicle/ProgressTracking';
+import VehicleTypeSelection from '@/components/sections/add-vehicle/VehicleTypeSelection';
+import { Car, Motorbike, Truck } from 'lucide-react';
+import { TbCarSuv } from 'react-icons/tb';
 
 export interface VehicleAddWizardType {
   nextStep: (e: SyntheticEvent) => void;
   prevStep: (e: SyntheticEvent) => void;
 }
+
+export interface VehicleTypesType  {
+    type: string;
+    icon: JSX.Element;
+    title: string;
+    subtitle: string;
+}[]
 
 const AddVehiclePage = () => {
   const [currentStep, setCurrentStep] = useState(0);
@@ -30,65 +40,101 @@ const AddVehiclePage = () => {
     setCurrentStep((prev) => prev - 1);
   };
 
+  const vehicleTypes: VehicleTypesType[] = [
+    {
+      type: 'two-wheels',
+      icon: <Motorbike  className='w-8 h-8'/>,
+      title: "MotorCycle",
+      subtitle: '2-wheeled motorized',
+    },
+    {
+      type: 'four-wheels',
+      icon: <Car className='w-8 h-8'/>,
+      title: "Car",
+      subtitle: '4-wheeled standard',
+    },
+    {
+      type: 'four-wheels',
+      icon: <TbCarSuv className='w-8 h-8'/>,
+      title: "SUV / 4X4",
+      subtitle: 'Sports utility vehicle',
+    },
+    {
+      type: 'four-wheels',
+      icon: <Truck className='w-8 h-8'/>,
+      title: "Truck / Van",
+      subtitle: 'Commercial vehicles',
+    },
+    {
+      type: 'four-wheels',
+      icon: <Truck className='w-8 h-8'/>,
+      title: "Bus / Minivan",
+      subtitle: 'Passenger transport',
+    },
+  ];
+
   const addVehicleSteps = [
-    'VEHICLE_TYPE',
-    'VEHICLE_BRAND',
-    'VEHICLE_MODEL',
-    'VEHICLE_VARIANT',
-    'VEHICLE_DETAILS',
-    'VEHICLE_PRICING',
-    'VEHICLE_LOCATION',
-    'VEHICLE_CONFIRM',
+    'TYPE',
+    'BRAND',
+    'MODEL',
+    'VARIANT',
+    'DETAILS',
+    'PRICING',
+    'LOCATION',
+    'CONFIRM',
   ];
   return (
-    <main className="text-white">
-      <h1>Add Vehicle</h1>
-      <p>some text here if necessary for UI {currentStep}</p>
+    <main className="bg-black min-h-screen py-5 px-8 text-white">
+      <h1 className="mt-1 text-2xl font-semibold">List a New Vehicle</h1>
+      <p className="mt-1 text-sm text-white/50">
+        Complete the steps below to add your vehicle to the platform.
+      </p>
 
+      <ProgressTracking steps={addVehicleSteps} currentStep={currentStep} />
       <section>
-        {addVehicleSteps[currentStep] == 'VEHICLE_TYPE' && (
-          <VehicleTypeSelction
+        {addVehicleSteps[currentStep] == 'TYPE' && (
+          <VehicleTypeSelection vehicleTypes={vehicleTypes}
             nextStep={moveToNextStep}
             prevStep={moveToPreviousStep}
           />
         )}
-        {addVehicleSteps[currentStep] == 'VEHICLE_BRAND' && (
+        {addVehicleSteps[currentStep] == 'BRAND' && (
           <VehicleBrandSelction
             nextStep={moveToNextStep}
             prevStep={moveToPreviousStep}
           />
         )}
-        {addVehicleSteps[currentStep] == 'VEHICLE_MODEL' && (
+        {addVehicleSteps[currentStep] == 'MODEL' && (
           <VehicleModelSelction
             nextStep={moveToNextStep}
             prevStep={moveToPreviousStep}
           />
         )}
-        {addVehicleSteps[currentStep] == 'VEHICLE_VARIANT' && (
+        {addVehicleSteps[currentStep] == 'VARIANT' && (
           <VehicleVariantSelction
             nextStep={moveToNextStep}
             prevStep={moveToPreviousStep}
           />
         )}
-        {addVehicleSteps[currentStep] == 'VEHICLE_DETAILS' && (
+        {addVehicleSteps[currentStep] == 'DETAILS' && (
           <VehicleDetailsSelction
             nextStep={moveToNextStep}
             prevStep={moveToPreviousStep}
           />
         )}
-        {addVehicleSteps[currentStep] == 'VEHICLE_PRICING' && (
+        {addVehicleSteps[currentStep] == 'PRICING' && (
           <VehiclePricingSelction
             nextStep={moveToNextStep}
             prevStep={moveToPreviousStep}
           />
         )}
-        {addVehicleSteps[currentStep] == 'VEHICLE_LOCATION' && (
+        {addVehicleSteps[currentStep] == 'LOCATION' && (
           <VehicleLocationSelction
             nextStep={moveToNextStep}
             prevStep={moveToPreviousStep}
           />
         )}
-        {addVehicleSteps[currentStep] == 'VEHICLE_CONFIRM' && (
+        {addVehicleSteps[currentStep] == 'CONFIRM' && (
           <VehicleConfirm
             nextStep={moveToNextStep}
             prevStep={moveToPreviousStep}
