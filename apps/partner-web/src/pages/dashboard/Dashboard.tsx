acf96f0f016857775b4cc56b2987660b76120223
Richu Sony <sonyrichu4@gmail.com>
2026-03-31 18:02:32 +0530

    ui: fixed dashboard min height issue

--- apps/partner-web/src/pages/dashboard/Dashboard.tsx
+++ apps/partner-web/src/pages/dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@ import DashboardCard from '@/components/helpers/DashboardCard';
 
 const Dashboard = () => {
   return (
-    <section className="w-full rounded-b-xl text-white">
+    <section className="w-full min-h-screen rounded-b-xl text-white">
       <div className="px-2 md:px-0 pt-5 flex flex-wrap items-center justify-center gap-5">
         <DashboardCard
           heading="Total Revenue"
